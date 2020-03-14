c78130a5c2b647fcbcfe8d369313cb1069767962
jovandosen <jovandosen1994@gmail.com>
2020-03-14 19:27:47 +0100

    Cards added

--- index.php
+++ index.php
@@ -307,6 +307,104 @@
   				<figcaption class="figure-caption">Description text</figcaption>
   			</figure>
 
+  			<div class="container">
+  				<div class="row">
+  					<div class="col">
+  						<div class="card" style="width: 18rem;">
+			  				<img src="images/image_test.jpg" class="card-img-top" alt="test image">
+			  				<div class="card-body">
+							    <h5 class="card-title">Card title</h5>
+							    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							    <a href="#" class="btn btn-primary">Go somewhere</a>
+			  				</div>
+						</div>
+  					</div>
+  					<div class="col">
+  						<div class="card" style="width: 18rem;">
+			  				<img src="images/image_test.jpg" class="card-img-top" alt="test image">
+			  				<div class="card-body">
+							    <h5 class="card-title">Card title</h5>
+							    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							    <a href="#" class="btn btn-primary">Go somewhere</a>
+			  				</div>
+						</div>
+  					</div>
+  					<div class="col">
+  						<div class="card" style="width: 18rem;">
+			  				<img src="images/image_test.jpg" class="card-img-top" alt="test image">
+			  				<div class="card-body">
+							    <h5 class="card-title">Card title</h5>
+							    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							    <a href="#" class="btn btn-primary">Go somewhere</a>
+			  				</div>
+						</div>
+  					</div>
+  				</div>
+  			</div>
+
+  			<div class="card bg-dark text-white">
+	  			<img src="images/img_forest.jpg" class="card-img" alt="foo image">
+		  		<div class="card-img-overlay">
+		    		<h5 class="card-title">Card title</h5>
+		    		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		    		<p class="card-text">Last updated 3 mins ago</p>
+		  		</div>
+			</div>
+
+			<div class="card-group">
+  				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+ 				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+  				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+			</div>
+
+			<div class="card-deck">
+  				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+  				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+  				<div class="card">
+    				<img src="images/image_test.jpg" class="card-img-top" alt="...">
+    				<div class="card-body">
+      					<h5 class="card-title">Card title</h5>
+      					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    				</div>
+  				</div>
+			</div>
+
 		</div>
 
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
