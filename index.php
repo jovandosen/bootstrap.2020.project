d0971a74d6c0808879d5567c0265eb11a1715916
jovandosen <jovandosen1994@gmail.com>
2020-03-14 20:09:49 +0100

    Carousel added

--- index.php
+++ index.php
@@ -405,6 +405,33 @@
   				</div>
 			</div>
 
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  			<ol class="carousel-indicators">
+	    			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	   	 			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  			</ol>
+  				<div class="carousel-inner">
+    				<div class="carousel-item active">
+      					<img src="images/mvc.png" class="d-block w-100" alt="...">
+    				</div>
+    				<div class="carousel-item">
+      					<img src="images/site.jpg" class="d-block w-100" alt="...">
+    				</div>
+    				<div class="carousel-item">
+      					<img src="images/image_test.jpg" class="d-block w-100" alt="...">
+    				</div>
+  				</div>
+	  			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    			<span class="sr-only">Previous</span>
+	  			</a>
+  				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Next</span>
+  				</a>
+			</div>
+
 		</div>
 
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
