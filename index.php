c1a6711c279e2474b2bc4f1c45e480a29f801282
jovandosen <jovandosen1994@gmail.com>
2020-03-15 14:12:59 +0100

    Tabs added

--- index.php
+++ index.php
@@ -478,6 +478,25 @@
   				</div>
 			</div>
 
+			<div class="row">
+  				<div class="col-4">
+    				<div class="list-group" id="list-tab" role="tablist">
+      					<a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+      					<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+      					<a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+      					<a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+    				</div>
+  				</div>
+  				<div class="col-8">
+    				<div class="tab-content" id="nav-tabContent">
+			      		<div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Foo</div>
+			      		<div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Bar</div>
+			      		<div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Baz</div>
+			      		<div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Test</div>
+    				</div>
+  				</div>
+			</div>
+
 		</div>
 
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
