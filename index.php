48be82f698ee5d0e6c4607ac3c4582d209df2630
jovandosen <jovandosen1994@gmail.com>
2020-03-18 13:11:03 +0100

    Image slider added

--- index.php
+++ index.php
@@ -42,6 +42,45 @@
 		</div>
 		</nav>
 
+		<!--Image Slider-->
+
+		<div id="slides" class="carousel slide" data-ride="carousel">
+			<ul class="carousel-indicators">
+				<li data-target="#slides" data-slide-to="0" class="active"></li>
+				<li data-target="#slides" data-slide-to="1"></li>
+				<li data-target="#slides" data-slide-to="2"></li>
+			</ul>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/sample.jpg" width="100%" height="500px">
+					<div class="carousel-caption">
+						<h1 class="display-2">Boostrap</h1>
+						<h3>Description text</h3>
+						<button type="button" class="btn btn-outline-light btn-lg">VIEW DEMO</button>
+						<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/site.jpg" width="100%" height="500px">
+					<div class="carousel-caption">
+						<h1 class="display-2">Boostrap</h1>
+						<h3>Description text</h3>
+						<button type="button" class="btn btn-outline-light btn-lg">VIEW DEMO</button>
+						<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="images/test.jpg" width="100%" height="500px">
+					<div class="carousel-caption">
+						<h1 class="display-2">Boostrap</h1>
+						<h3>Description text</h3>
+						<button type="button" class="btn btn-outline-light btn-lg">VIEW DEMO</button>
+						<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
