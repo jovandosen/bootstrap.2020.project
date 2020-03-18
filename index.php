dec3db64618e117493e64fb2325e8eb8584d30cc
jovandosen <jovandosen1994@gmail.com>
2020-03-18 13:38:58 +0100

    Welcome Section added

--- index.php
+++ index.php
@@ -94,6 +94,20 @@
 			</div>
 		</div>
 
+		<!--Welcome Section-->
+
+		<div class="container-fluid">
+			<div class="row welcome text-center">
+				<div class="col-12">
+					<h1 class="display-4">Built with ease.</h1>
+				</div>
+				<hr>
+				<div class="col-12">
+					<p class="lead">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+				</div>
+			</div>
+		</div>
+
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
