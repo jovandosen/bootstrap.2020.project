14ea4e67a9f55182808a8d2200f35af45f5cf03e
jovandosen <jovandosen1994@gmail.com>
2020-03-19 14:48:32 +0100

    About page content updated

--- about.php
+++ about.php
@@ -83,7 +83,7 @@
 		<div class="wrap-elements">
 			<div class="container-fluid">
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-md-6 col-lg-3">
 						<div class="card special">
 	  						<img src="images/site.jpg" class="card-img-top" alt="foo">
 	  						<div class="card-body">
@@ -93,7 +93,7 @@
 	  						</div>
 						</div>
 					</div>
-					<div class="col-md-3">
+					<div class="col-md-6 col-lg-3">
 						<div class="card special">
 	  						<img src="images/img_forest.jpg" class="card-img-top" alt="foo">
 	  						<div class="card-body">
@@ -103,7 +103,7 @@
 	  						</div>
 						</div>
 					</div>
-					<div class="col-md-3">
+					<div class="col-md-6 col-lg-3">
 						<div class="card special">
 	  						<img src="images/site.jpg" class="card-img-top" alt="foo">
 	  						<div class="card-body">
@@ -113,7 +113,7 @@
 	  						</div>
 						</div>
 					</div>
-					<div class="col-md-3">
+					<div class="col-md-6 col-lg-3">
 						<div class="card special">
 	  						<img src="images/img_forest.jpg" class="card-img-top" alt="foo">
 	  						<div class="card-body">
@@ -127,6 +127,84 @@
 			</div>
 			<hr>
 		</div>
+
+		<div class="wrap-elements">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-6 add-padding">
+						
+						<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+	  						<div class="carousel-inner">
+	    						<div class="carousel-item active">
+	      							<img src="images/image_test.jpg" class="d-block w-100" alt="foo" width="100%" height="400px">
+	    						</div>
+	    						<div class="carousel-item">
+	      							<img src="images/test.jpg" class="d-block w-100" alt="bar" width="100%" height="400px">
+	    						</div>
+	    						<div class="carousel-item">
+	      							<img src="images/site.jpg" class="d-block w-100" alt="baz" width="100%" height="400px">
+	    						</div>
+	  						</div>
+						  	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						    	<span class="sr-only">Previous</span>
+						  	</a>
+	  						<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	    						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    						<span class="sr-only">Next</span>
+	  						</a>
+						</div>
+
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-6">
+						<h3>Example title text</h3>
+						<p class="text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+						<br>
+						<p class="text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+					</div>
+				</div>
+			</div>
+			<hr>
+		</div>
+
+		<!--Footer-->
+
+		<footer>
+			<div class="container-fluid padding">
+				<div class="row text-center">
+					<div class="col-md-4">
+						<img src="images/image_test.jpg" width="200" height="54">
+						<hr class="light">
+						<p>555-555-5555</p>
+						<p>test@test.com</p>
+						<p>200 Example Street</p>
+						<p>Some place nice</p>
+					</div>	
+					<div class="col-md-4">
+						<hr class="light">
+						<h5>Our Hours</h5>
+						<hr class="light">
+						<p>Monday: 9am - 5pm</p>
+						<p>Saturday: 10am - 4pm</p>
+						<p>Sunday: closed</p>
+						<p>Some place nice</p>
+					</div>	
+					<div class="col-md-4">
+						<hr class="light">
+						<h5>Service Test</h5>
+						<hr class="light">
+						<p>Monday: 9am - 5pm</p>
+						<p>Saturday: 10am - 4pm</p>
+						<p>Some place nice</p>
+						<p>Some place nice</p>
+					</div>
+					<div class="col-12">
+						<hr class="light-100">
+						<h5>&copy; johndoe.com</h5>
+					</div>		
+				</div>
+			</div>
+		</footer>
 		
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
