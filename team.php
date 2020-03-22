68d3817e17dc69947e3f6bf07182979b00a19cb4
jovandosen <jovandosen1994@gmail.com>
2020-03-22 15:57:18 +0100

    Team page content updated

--- team.php
+++ team.php
@@ -58,6 +58,107 @@
 			</div>
 		</div>
 
+		<div class="some-container-box">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 margin-bottom-data">						
+						<div class="card bg-dark text-white">
+					  		<img src="images/img_forest.jpg" class="card-img" alt="test1">
+					  		<div class="card-img-overlay">
+					    		<h5 class="card-title">Card title</h5>
+				    			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					    		<p class="card-text">Last updated 3 mins ago</p>
+					  		</div>
+						</div>
+					</div>
+					<div class="col-md-4 margin-bottom-data">
+						<div class="card bg-dark text-white">
+					  		<img src="images/img_forest.jpg" class="card-img" alt="test2">
+					  		<div class="card-img-overlay">
+					    		<h5 class="card-title">Card title</h5>
+				    			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					    		<p class="card-text">Last updated 3 mins ago</p>
+					  		</div>
+						</div>
+					</div>
+					<div class="col-md-4 margin-bottom-data">
+						<div class="card bg-dark text-white">
+					  		<img src="images/img_forest.jpg" class="card-img" alt="test3">
+					  		<div class="card-img-overlay">
+					    		<h5 class="card-title">Card title</h5>
+				    			<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					    		<p class="card-text">Last updated 3 mins ago</p>
+					  		</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</div>
+
+		<div class="some-container-box">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+							<div class="col-12 link-margin-data">
+								<a href="#">This is some link text to be displayed This is some link text to be displayed</a>
+								<hr>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="card">
+							<div class="card-header">
+								<b>Development</b>
+							</div>
+							<div class="card-body">
+								<ul class="list-group" id="links-wrapper">
+									<li><a href="#">This is some link text to be displayed</a></li>
+									<li><a href="#">This is some link text to be displayed</a></li>
+									<li><a href="#">This is some link text to be displayed</a></li>
+									<li><a href="#">This is some link text to be displayed</a></li>
+									<li><a href="#">This is some link text to be displayed</a></li>
+								</ul>
+							</div>
+							<div class="card-footer text-right">
+								Footer text
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<hr>
+			</div>
+		</div>
+
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
